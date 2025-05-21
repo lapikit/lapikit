e74cc82af153f7fc1fe6e645bf69b70cd5dcbb3d
nycolaide <laurent@nycolaide.dev>
2025-05-21 19:17:07 +0200

    update tailwind version + change logic for css device + bugfix css navbar

--- src/style/parser/device.ts
+++ src/style/parser/device.ts
@@ -1,4 +1,5 @@
 import type { Lapikit } from '$lib/internal/types.js';
+import { setUnit } from '$lib/internal/unit.js';
 
 export const devices = (config: Lapikit) => {
 	let css = ``;
@@ -8,31 +9,23 @@ export const devices = (config: Lapikit) => {
 		laptop: config.breakpoints.laptopBreakpoint
 	};
 
-	for (const [key, value] of Object.entries(list)) {
-		const breakpointValue = config.breakpoints.thresholds[value];
-		const breakpointValueMax =
-			key !== 'laptop'
-				? config.breakpoints.thresholds[
-						key === 'mobile' ? list.tablet : key == 'tablet' ? list.laptop : ''
-					]
-				: undefined;
+	css += `@media screen and (max-width: ${setUnit(config.breakpoints.thresholds[list.tablet])}) {\n`;
+	css += `.hidden-mobile {\n`;
+	css += `display: none !important;\n`;
+	css += `}\n`;
+	css += `}\n`;
 
-		css += `.device-${key}, .only-on-${key} {\n`;
-		css += `display: none;\n`;
-		css += `}\n`;
+	css += `@media screen and (min-width: ${setUnit(config.breakpoints.thresholds[list.tablet])}) and (max-width: ${setUnit(config.breakpoints.thresholds[list.laptop])})  {\n`;
+	css += `.hidden-tablet {\n`;
+	css += `display: none !important;\n`;
+	css += `}\n`;
+	css += `}\n`;
 
-		css += `@media screen and (min-width: ${typeof breakpointValue === 'number' ? breakpointValue + 'px' : breakpointValue}) {\n`;
-		css += `.device-${key} {\n`;
-		css += `display: inherit !important;\n`;
-		css += `}\n`;
-		css += `}\n`;
-
-		css += `@media screen and (min-width: ${typeof breakpointValue === 'number' ? breakpointValue + 'px' : breakpointValue}) ${breakpointValueMax ? `and (max-width: ${typeof breakpointValueMax === 'number' ? breakpointValueMax + 'px' : breakpointValueMax})` : ''}{\n`;
-		css += `.only-on-${key} {\n`;
-		css += `display: inherit !important;\n`;
-		css += `}\n`;
-		css += `}\n`;
-	}
+	css += `@media screen and (min-width: ${setUnit(config.breakpoints.thresholds[list.laptop])})  {\n`;
+	css += `.hidden-laptop {\n`;
+	css += `display: none !important;\n`;
+	css += `}\n`;
+	css += `}\n`;
 
 	return css;
 };
