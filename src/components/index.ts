59f613af4852302e7a583d676b8b0bc801eabf7d
nycolaide <laurent@nycolaide.dev>
2025-05-22 16:44:27 +0200

    delete navbar component

--- src/components/index.ts
+++ src/components/index.ts
@@ -20,4 +20,3 @@ export { default as Card } from './card/card.svelte';
 export { default as Toolbar } from './toolbar/toolbar.svelte';
 export { default as Appbar } from './appbar/appbar.svelte';
 export { default as Spacer } from './spacer/spacer.svelte';
-export { default as Navbar } from './navbar/navbar.svelte';
