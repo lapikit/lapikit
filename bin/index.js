0d64366452c7c38b2a1051907d426de94caeeab1
Nycolaide <laurent@nycolaide.dev>
2025-08-15 22:49:30 +0200

    rewrite code for current ci install lapikit

--- bin/index.js
+++ bin/index.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 import prompts from 'prompts';
 import { ansi, terminal } from './helper.js';
+import { legacyConfiguration } from './legacy.js';
 
 async function main() {
 	console.log('  _                 _ _    _ _   ');
@@ -12,7 +13,7 @@ async function main() {
 	console.log('             | |                 ');
 	console.log('             |_|                 \n');
 
-	terminal('none', `${ansi.bold.blue('LAPIKIT')} - Component Library for Svelte\n\n`);
+	terminal('none', `${ansi.bold.blue('Lapikit')} - Component Library for Svelte\n\n`);
 
 	const { confirm } = await prompts({
 		type: 'toggle',
@@ -27,6 +28,56 @@ async function main() {
 		console.log('❌ Installation canceled. See you soon.');
 		process.exit(0);
 	}
+	// temps with legacy and new process install
+	const { type } = await prompts({
+		type: 'select',
+		name: 'type',
+		message: 'Select installation type:',
+		choices: [
+			{ title: 'Classic install with lapikit.config.js', value: 'current' },
+			{
+				title: 'Preview install with new plugin/lapikit.(js|ts) <experimental>',
+				value: 'experimental'
+			}
+		]
+	});
+
+	if (type === 'current') {
+		// Classic install
+		const settings = await prompts([
+			{
+				type: 'text',
+				name: 'pathCSS',
+				message: 'Where would you like to import the lapikit CSS files?',
+				initial: 'src/app.css',
+				validate: (value) =>
+					value.startsWith('src/') ? true : 'Please provide a valid path starting with src/'
+			}
+		]);
+
+		await legacyConfiguration(settings);
+	} else if (type === 'experimental') {
+		// Preview install
+	}
+
+	// const settings = await prompts([
+	// 	{
+	// 		type: 'text',
+	// 		name: 'pathConfig',
+	// 		message: 'Where would you like to install the lapikit configuration files?',
+	// 		initial: 'src/plugins',
+	// 		validate: (value) =>
+	// 			value.startsWith('src/') ? true : 'Please provide a valid path starting with src/'
+	// 	},
+	// 	{
+	// 		type: 'toggle',
+	// 		name: 'typescript',
+	// 		message: 'Use TypeScript?',
+	// 		initial: true,
+	// 		active: 'Yes',
+	// 		inactive: 'No'
+	// 	}
+	// ]);
 
 	// const response = await prompts([
 	// 	{
---
+++ bin/legacy.js
@@ -0,0 +1,33 @@
+#!/usr/bin/env node
+import { promises as fs } from 'fs';
+import path from 'path';
+import { ansi, terminal, envTypescript } from './helper.js';
+import { adapterCSSConfig, adapterViteConfig } from './modules/adapter.js';
+
+export async function legacyConfiguration(options) {
+	const typescriptEnabled = envTypescript();
+	const configPath = path.resolve(process.cwd(), 'lapikit.config.js');
+
+	try {
+		await fs.writeFile(configPath, preset.trim() + '\n', 'utf8');
+		terminal('success', `has create lapikit.config.js on your project.`);
+	} catch (error) {
+		terminal('error', `failed to create configuration file:\n\n ${error}`);
+		terminal(
+			'warn',
+			`you can create lapikit.config.js manually, please visite https://lapikit.dev/docs/getting-started for more information`
+		);
+	}
+
+	await adapterViteConfig(typescriptEnabled);
+	await adapterCSSConfig(options);
+
+	terminal(
+		'info',
+		`${ansi.bold.blue('Thank to use lapikit, discover all posibility with lapikit on https://lapikit.dev')}\n\n`
+	);
+
+	console.log('Website: https://lapikit.dev');
+	console.log('Github: https://github.com/nycolaide/lapikit');
+	console.log('Support the developement: https://buymeacoffee.com/nycolaide');
+}
