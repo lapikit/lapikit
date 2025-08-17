d6d4beced9740a4d60d7b6e05617c441f62ad646
Nycolaide <laurent@nycolaide.dev>
2025-08-17 22:35:39 +0200

    add temporaly import for test ci

--- bin/presets.js
+++ bin/presets.js
@@ -1,13 +1,19 @@
 function presets() {
 	let content = '';
 
-	content += `/**
-        * Lapikit
-        * Library documentation: https://lapikit.dev
-        */\n\n`;
+	content += `/**`;
+	content += `\tLapikit`;
+	content += `\tLibrary documentation: https://lapikit.dev`;
+	content += ` */\n\n`;
+
 	content += `// Styles\n`;
-	content += `import 'lapikit/css'\n`;
+	content += `import 'lapikit/css';\n\n`;
+
+	content += `// Composables\n`;
+	content += `import { helloWorld } from 'lapikit';\n\n`;
+
 	content += `// https://lapikit.dev/docs/getting-started\n`;
+	content += `export default helloWorld();`;
 
 	return content;
 }
