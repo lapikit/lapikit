5613480eac20660f95c8072e44f2ecf740d87443
Nycolaide <laurent@nycolaide.dev>
2025-10-15 20:56:19 +0200

    fix error linter

--- src/internal/helpers/scroll.ts
+++ src/internal/helpers/scroll.ts
@@ -1,5 +1,5 @@
 export function disabledScroll(state: boolean) {
-	if (typeof window !== "undefined" && typeof document !== "undefined") {
+	if (typeof window !== 'undefined' && typeof document !== 'undefined') {
 		document.body.style.overflow = state ? 'hidden' : '';
 	}
 }
