5d2f3a889108e730f87d74c7b97271d556b2ee1b
Nycolaide <laurent@nycolaide.dev>
2026-03-11 15:01:14 +0100

    add new component popover and dropdown

--- src/lib/labs/compiler/components.ts
+++ src/lib/labs/compiler/components.ts
@@ -1,3 +1,11 @@
-const lapikitComponents: readonly string[] = ['sheet', 'app', 'btn', 'icon', 'avatar'] as const;
+const lapikitComponents: readonly string[] = [
+	'sheet',
+	'app',
+	'btn',
+	'icon',
+	'avatar',
+	'dropdown',
+	'popover'
+] as const;
 
 export default lapikitComponents;
--- src/lib/labs/components/index.ts
+++ src/lib/labs/components/index.ts
@@ -4,3 +4,5 @@ export { default as KitApp } from './app/app.svelte';
 export { default as KitBtn } from './btn/btn.svelte';
 export { default as KitIcon } from './icon/icon.svelte';
 export { default as KitAvatar } from './avatar/avatar.svelte';
+export { default as KitDropdown } from './dropdown/dropdown.svelte';
+export { default as KitPopover } from './popover/popover.svelte';
