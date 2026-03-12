b02616dc8b24c4f21fbbb4d2299f9799feadc05f
Nycolaide <laurent@nycolaide.dev>
2026-03-12 18:10:31 +0100

    Revert "add new component chip"

--- src/lib/labs/components/index.ts
+++ src/lib/labs/components/index.ts
@@ -3,7 +3,6 @@ export { default as KitSheet } from './sheet/sheet.svelte';
 export { default as KitApp } from './app/app.svelte';
 export { default as KitBtn } from './btn/btn.svelte';
 export { default as KitIcon } from './icon/icon.svelte';
-export { default as KitChip } from './chip/chip.svelte';
 export { default as KitAvatar } from './avatar/avatar.svelte';
 export { default as KitSpacer } from './spacer/spacer.svelte';
 export { default as KitSeparator } from './separator/separator.svelte';
