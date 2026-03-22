6e40cb7dc4ab2780bdff238975e208c8764d9165
Nycolaide <laurent@nycolaide.dev>
2026-03-22 22:01:23 +0100

    fix error to install lapikit with new core lili

--- src/lib/entry-bundler.ts
+++ src/lib/entry-bundler.ts
@@ -1,7 +1,8 @@
 import { liliCore } from '$lib/framework';
 import type { LapikitPreprocessOptions } from '$lib/@types';
+import type { PreprocessorGroup } from 'svelte/compiler';
 
-function lapikitPreprocess(options?: LapikitPreprocessOptions) {
+function lapikitPreprocess(options?: LapikitPreprocessOptions): PreprocessorGroup {
 	return liliCore(options);
 }
 
