d7cb1a0ffeae6d0a30fdef51778396b59866a499
Nycolaide <laurent@nycolaide.dev>
2026-04-26 19:57:38 +0200

    WIP  add new type for handleEvent on Dropdown

--- src/lib/components/dropdown/dropdown.types.ts
+++ src/lib/components/dropdown/dropdown.types.ts
@@ -13,6 +13,11 @@ export type ModelDropdownProps = {
 	toggle: (element: HTMLElement | PointerEvent | null) => void;
 };
 
+export type ModelDropdownHandleProps = {
+	state: 'open' | 'close';
+	element: (element: HTMLElement | PointerEvent | null) => void;
+};
+
 export interface DropdownProps extends Component {
 	ref?: HTMLElement | null;
 	rounded?: RoundedType | string;
